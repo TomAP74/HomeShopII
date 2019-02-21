30745ce5d6caa42489de1becfa1972c364d4a7c0
TomAP74 <pons_thomas@orange.fr>
2019-02-21 15:58:14 +0100

    New project HomeShopII

---
+++ src/com/ocr/thomas/Bill.java
@@ -0,0 +1,4 @@
+package com.ocr.thomas;
+
+public class Bill {
+}
---
+++ src/com/ocr/thomas/Customer.java
@@ -0,0 +1,14 @@
+package com.ocr.thomas;
+
+public class Customer {
+    String fullname;
+    String address;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
